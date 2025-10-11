e0cc1da8b641e25ff47bdc4849887d208d5b3cb2
AndyFM806 <leefernandez1245@gmail.com>
2025-10-10 19:16:04 -0500

    feat: redis con volumen (iac/redis.tf)

--- iac/redis.tf
+++ iac/redis.tf
@@ -1,19 +1,21 @@
 resource "docker_image" "redis" {
-  name = "redis:7.4.1-alpine"
+  name = var.redis_image
+}
+
+resource "docker_volume" "redis_data" {
+  name = "redis_data"
 }
 
 resource "docker_container" "redis" {
-  name  = "redis-${terraform.workspace}"
-  image = docker_image.redis.image_id
-
+  name  = "redis"
+  image = docker_image.redis.latest
+  ports {
+    internal = 6379
+    external = var.redis_external_port
+  }
+  volumes = ["${docker_volume.redis_data.name}:/data"]
   networks_advanced {
     name = docker_network.persistence_net.name
   }
-
-  ports {
-    internal = 6379
-    external = var.redis_external_port[terraform.workspace]
-  }
-
-  restart = "always"
 }
+
