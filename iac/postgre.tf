d4afc7a65bf54fcc5efe2a6245e6956a849a38e9
AndyFM806 <leefernandez1245@gmail.com>
2025-10-10 19:18:07 -0500

    feat: postgresql con volumen y env (iac/postgre.tf)

--- iac/postgre.tf
+++ iac/postgre.tf
@@ -1,26 +1,25 @@
 resource "docker_image" "postgres" {
-  name         = "postgres:15-alpine"
-  keep_locally = false
+  name = var.postgres_image
+}
+
+resource "docker_volume" "pgdata" {
+  name = "pgdata"
 }
 
 resource "docker_container" "postgres" {
-  name  = "postgres-${terraform.workspace}"
-  image = docker_image.postgres.image_id
-
-  networks_advanced {
-    name = docker_network.persistence_net.name
-  }
-
-  ports {
-    internal = 5432
-    external = var.postgres_external_port
-  }
-
+  name  = "postgres"
+  image = docker_image.postgres.latest
   env = [
     "POSTGRES_USER=${var.postgres_user}",
     "POSTGRES_PASSWORD=${var.postgres_password}",
     "POSTGRES_DB=${var.postgres_db}"
   ]
-
-  restart = "always"
+  ports {
+    internal = 5432
+    external = var.postgres_external_port
+  }
+  volumes = ["${docker_volume.pgdata.name}:/var/lib/postgresql/data"]
+  networks_advanced {
+    name = docker_network.persistence_net.name
+  }
 }
